a185a7b69248f5e4c236b9f988501bd77926cf8c
jmicharkCode <88967855+jmicharkCode@users.noreply.github.com>
2021-12-12 12:24:40 +0700

    Update Bai3.java

--- src/Bai3.java
+++ src/Bai3.java
@@ -1,12 +1,18 @@
 
-
 public class Bai3 {
+
 	public static void main(String[] args) {
-		System.out.println("Java Version: 1.8.0_71");
-		System.out.println("Java Runtime Version: 1.8.0_71-b15");
-		System.out.println("Java Home: /opt/jdk/jdk1.8.0_71/jre");
-		System.out.println("Java Vendor: Oracle Corporation");
-		System.out.println("Java Vendor URL: http://Java. oracle. com/");
-		System.out.println("Java Class Path: .");
+		System.out.println("Java Runtime Version: " +System.getProperty("java.runtime.version"));
+		String version = System.getProperty("java.version");
+		System.out.println("Java version: " + version);
+		String vm = System.getProperty("java.home");
+		System.out.println("Java Home: " + vm);
+		String vendor = System.getProperty("java.vendor");
+		System.out.println("Java Vendor: " + vendor);
+		String vendorUrl = System.getProperty("java.vendor.url");
+		System.out.println("Java Vendor URL: " + vendorUrl);
+		String classPath = System.getProperty("java.clash.path");
+		System.out.println("Java Class Path: " + classPath);
+	
 	}
 }
